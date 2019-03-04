dad8bf6143e6d7a2352ad109fd75208adef30aa2
Olav Schuermann <developer@netzpluslogik.de>
2019-03-04 15:09:04 +0100

    project part 2

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,12 +1,10 @@
-const blankLine = '  |   |  ';
-console.log("This is what an empty board would look like:");
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+const printBoard = board => {
+    console.log('Current Board: ');
+    console.log(board[0]);
+} 
 
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
-console.log("This is what a board with a guess and a bomb on it would look like:");
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const board = [ [' | '], 
+                [' | '], 
+                [' | '] ];
+
+console.log(board);
