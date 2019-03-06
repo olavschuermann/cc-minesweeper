c20043ad6a4e6fa0a4f2e83a59129e0caf4a47fc
Olav Schuermann <developer@netzpluslogik.de>
2019-03-06 14:50:24 +0100

    project part 3 work in progress

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,19 +1,9 @@
-const exampleBoard = [ 
-                [' ', ' ', ' '], 
-                [' ', ' ', ' '], 
-                [' ', ' ', ' '] 
-            ];
-
-const printBoard = board => {
-    console.log('Current Board: ');
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
-};             
-
-printBoard(exampleBoard);
-
-exampleBoard[0][1] = '1';
-exampleBoard[1][0] = 'B';
-
-printBoard(exampleBoard);
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    const board = [];
+    for(i = 0; i <= numberOfRows; i++) {
+        let row = [];
+        for (col = 0; col <= numberOfColumns; col++) {
+            row.push(' ');
+        }
+    }
+}
