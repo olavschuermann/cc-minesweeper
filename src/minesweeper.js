fa8b69792ebef175e43f5dae4f8eac77a00a632a
Olav Schuermann <developer@netzpluslogik.de>
2019-03-06 15:10:54 +0100

    project part 2 up to 24

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,9 +1,34 @@
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
     const board = [];
-    for(i = 0; i <= numberOfRows; i++) {
+    for(i = 0; i < numberOfRows; i++) {
         let row = [];
-        for (col = 0; col <= numberOfColumns; col++) {
+        for (col = 0; col < numberOfColumns; col++) {
             row.push(' ');
         }
+        board.push(row);
     }
+    return board;
+}
+
+// console.log(generatePlayerBoard(4, 4));
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    const board = [];
+    for(i = 0; i < numberOfRows; i++) {
+        let row = [];
+        for (col = 0; col < numberOfColumns; col++) {
+            row.push(null);
+        }
+        board.push(row);
+    }
+    let numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced < numberOfBombs) {
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        board[randomRowIndex][randomColumnIndex] = 'B';
+    }
+    
+
+    
+    return board;   
 }
