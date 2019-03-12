09c846d61dbb54d467d6f11ed3693911c3ca1ae3
Olav Schuermann <developer@netzpluslogik.de>
2019-03-12 15:34:08 +0100

    project part 5

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -5,6 +5,23 @@ class Board {
         this._playerBoard = Board.generatePlayerBoard(numberOfRows, numberOfColumns);
         this._bombBoard = Board.generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs);
     }
+    get playerBoard() {
+        return this._playerBoard;
+    }
+
+    flipTile() {
+        flipTile = (rowIndex, columnIndex) => {
+            if(this._playerBoard[rowIndex][columnIndex] !== ' ') {
+                console.log('This tile has already been flipped!');
+                return;
+            } else if (this._bombBoard[rowIndex][columnIndex] === 'B') {
+                this._playerBoard[rowIndex][columnIndex] = 'B';
+            } else {
+                this._playerBoard[rowIndex][columnIndex] = this.getNumberOfNeighborBombs(rowIndex, columnIndex);
+            }
+        }
+        this._numberOfTiles--;
+    }
 }
 
 
@@ -78,16 +95,7 @@ const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
     return numberOfBombs;
 };
 
-const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
-    if(playerBoard[rowIndex][columnIndex] !== ' ') {
-        console.log('This tile has already been flipped!');
-        return;
-    } else if (bombBoard[rowIndex][columnIndex] === 'B') {
-        playerBoard[rowIndex][columnIndex] = 'B';
-    } else {
-        playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
-    }
-}
+
 
 const printBoard = board => {
     console.log(board.map(row  => row.join(' | ')).join('\n'));
